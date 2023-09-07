91582c0e369fb7fcad7cc055bd7fcc83f227308d
n-tilde <tropicaldata@gmail.com>
2023-09-07 17:29:11 -0300

    Includes styled components from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import React, { useState, useRef, ReactNode } from "react";
 
 type todo = {
   id: number;
   content: string;
   finished: number;
 };
+
+function TodoCard({ todo }: { todo: todo }) {
+  return (
+    <Card className="w-[350px] bg-black text-primary-foreground mb-2">
+      <CardContent className="flex flex-row p-4">
+        <div className="flex justify-center align-center w-8 mr-2">
+          <Checkbox className="m-auto" />
+        </div>
+        <div>
+          <p className="text-sm font-medium leading-none">{todo.content}</p>
+          <p className="text-sm text-muted-foreground">
+            {String(todo.id).padStart(5, "0")}
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function Home() {
   console.log("✨ Evaluating");
@@ -30,14 +57,27 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={createTodo}>CREATE</button>
-      <button onClick={fetchTodos}>FETCH</button>
-      <input type="text" ref={inputRef} />
+    <main className="bg-black flex min-h-screen max-h-screen overflow-hidden flex-col items-center justify-between p-12">
       <div>
-        {todos.map((todo) => {
-          return <p>{JSON.stringify(todo)}</p>;
-        })}
+        <Card className="w-[350px] bg-black text-primary-foreground mb-2">
+          <CardHeader>
+            <Input
+              placeholder="Task"
+              type="text"
+              ref={inputRef}
+              className="bg-black"
+            />
+          </CardHeader>
+          <CardFooter className="flex flex-row justify-between">
+            <Button onClick={createTodo}>CREATE</Button>
+            <Button onClick={fetchTodos}>FETCH</Button>
+          </CardFooter>
+        </Card>
+        <div className="overflow-auto h-96">
+          {todos.map((todo) => (
+            <TodoCard todo={todo} />
+          ))}
+        </div>
       </div>
     </main>
   );
